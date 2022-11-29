12b91b402ffe89b52915a6e361a55dc09178dce5
wangbin <wangbin@daige.com.cn>
2022-11-29 11:20:34 +0800

    配置代码生成器

---
+++ bin-data/src/main/java/com/core/Main.java
@@ -0,0 +1,12 @@
+package com.core;
+
+/**
+ * @author wb
+ * @description:
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
